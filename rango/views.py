fcef7617c1b0b40894e9e53561d9fb49a4809163
Rishi Vinod <2331751v@student.gla.ac.uk>
2019-01-25 15:44:38 +0000

    fourth commit

--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    ##return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>" )
+    
 
     ## Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
@@ -16,12 +16,14 @@ def index(request):
     # Note that the first parameter is the template we wish to use.
 
     return render(request, 'rango/index.html', context=context_dict)
+    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>" )
     
    
 
 def about(request):
     context_dict = {'boldmessage': "Cat = Black"}
     return render(request, 'rango/about.html', context=context_dict)
+    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/'>Index</a>" )
     
     
 

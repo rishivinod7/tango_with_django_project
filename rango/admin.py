f871775e64f666cc9ed1cfa3fca005194a514766
Rishi Vinod <2331751v@student.gla.ac.uk>
2019-02-05 23:46:53 +0000

    9th Chapter

--- rango/admin.py
+++ rango/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 # Register your models here.
-
+admin.site.register(UserProfile)
 admin.site.register(Category)
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title','category', 'url')
-admin.site.register(Page,PageAdmin)
+admin.site.register(Page,PageAdmin)
+
 
 # Add in this class to customise the Admin Interface
 class CategoryAdmin(admin.ModelAdmin):
@@ -14,4 +16,5 @@ class CategoryAdmin(admin.ModelAdmin):
 
 # Update the registration to include this customised interface
 admin.site.unregister(Category)
-admin.site.register(Category, CategoryAdmin)
+admin.site.register(Category, CategoryAdmin)
+
--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import Category, Page, UserProfile
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,
@@ -32,7 +33,8 @@ class PageForm(forms.ModelForm):
         # we can either exclude the category field from the form,
         exclude = ('category',)
         # or specify the fields to include (i.e. not include the category field)
-        # fields = ('title', 'url', 'views')
+        # fields = ('title', 'url', 'views')
+
 
     def clean(self):
         cleaned_data = self.cleaned_data
@@ -44,4 +46,15 @@ class PageForm(forms.ModelForm):
             url = 'http://' + url
             cleaned_data['url'] = url
 
-            return cleaned_data
+            return cleaned_data
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
